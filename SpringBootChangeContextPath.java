578bc13dcf60f9e910fcd6b9402ad45042b32739
larinpaul <www.larinpavel@mail.ru>
2025-04-11 06:38:38 +0300

    // 2025 04 11
    // https://www.baeldung.com/spring-boot-context-path
    
    // Last updated May 11, 2024
    
    // Written by: baeldung
    // Reviewed by: Predrag Marić
    
    // 1. Overview
    
    // Spring Boot, by default, serves content on the root context path ("/").

--- SpringBootChangeContextPath.java
+++ SpringBootChangeContextPath.java
@@ -2,6 +2,15 @@
 
 // https://www.baeldung.com/spring-boot-context-path
 
+// Last updated May 11, 2024
+
+// Written by: baeldung
+// Reviewed by: Predrag Marić
+
+
+// 1. Overview
+
+// Spring Boot, by default, serves content on the root context path ("/").
 
 
 
